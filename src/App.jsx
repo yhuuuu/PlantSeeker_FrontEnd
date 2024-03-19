8b5cb64aea7de20bd39499dac5f5172894abbb3a
Yuting Hu <yuting.hu.careers@gmail.com>
2024-03-19 16:47:00 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import About from './components/about/About'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <About/>
+    </>
+  )
+}
+
+export default App

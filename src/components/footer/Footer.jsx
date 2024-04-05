c80f02ec309860425bd101452650e0b3f789423f
Yuting Hu <yuting.hu.careers@gmail.com>
2024-04-05 14:49:29 -0700

    added link to footer <a> tag

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,13 @@
 
 import './footer.css'
-
 function Footer() {
         return (
                 <div className='container footer-container'>
                         <footer class="py-3 my-4">
                                 <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                                        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><h6>Contact</h6></a></li>
-                                        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><h6>About</h6></a></li>
-                                        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><h6>Credits</h6></a></li>
+                                        <li class="nav-item"><a href="contact" class="nav-link px-2 text-body-secondary"><h6>Contact</h6></a></li>
+                                        <li class="nav-item"><a href="https://www.linkedin.com/in/yutinghu3/" class="nav-link px-2 text-body-secondary"><h6>About</h6></a></li>
+                                        <li class="nav-item"><a href="credits" class="nav-link px-2 text-body-secondary"><h6>Credits</h6></a></li>
                                 </ul>
                                 <p class="text-center text-body-secondary">© 2024 Plant Seeker, Inc</p>
                         </footer>
